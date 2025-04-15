3e6ec989193fcc15fe353da9f639cb391b7c1ccd
dhxogud <hyong0117@naver.com>
2025-04-15 15:24:35 +0900

    강사님꺼 새로 받아옴

--- Main.cpp
+++ Main.cpp
@@ -9,28 +9,7 @@ using namespace std;
 
 int main()
 {
-	std::vector<int> IntArray;
-
-	IntArray.push_back(1);
-	IntArray.push_back(2);
-	IntArray.push_back(3);
-	IntArray.push_back(4);
-	IntArray.push_back(5);
-	IntArray.push_back(6);
-	IntArray.push_back(7);
-	IntArray.erase(find(IntArray.begin(), IntArray.end(), 2));
-	IntArray.clear();
-
-	for (int i = 0; i < IntArray.size(); i++)
-	{
-		cout << IntArray[i] << endl;
-	}
-
-	for (auto data : IntArray)
-	{
-		cout << data << endl;
-	}
-
+	
 	return 0;
 }
 
