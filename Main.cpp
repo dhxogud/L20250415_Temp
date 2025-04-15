a95c8d4bedb18afb6a223e01313d0d504c20292a
dhxogud <hyong0117@naver.com>
2025-04-15 14:59:53 +0900

    vector 자료구조형과 iter 에 대한설명

--- Main.cpp
+++ Main.cpp
@@ -3,46 +3,34 @@
 #include "AGoblin.h"
 #include "ABoar.h"
 #include "ASlime.h"
-
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+	std::vector<int> IntArray;
 
-	APlayerCharacter MyPlayer = APlayerCharacter();
-	ABoar MyBoar = ABoar();
-	ASlime MySlime = ASlime();
-	AGoblin MyGoblin = AGoblin();
+	IntArray.push_back(1);
+	IntArray.push_back(2);
+	IntArray.push_back(3);
+	IntArray.push_back(4);
+	IntArray.push_back(5);
+	IntArray.push_back(6);
+	IntArray.push_back(7);
+	IntArray.erase(find(IntArray.begin(), IntArray.end(), 2));
+	IntArray.clear();
 
+	for (int i = 0; i < IntArray.size(); i++)
+	{
+		cout << IntArray[i] << endl;
+	}
 
-	int DestX = 10;
-	int DestY = 10;
-	// 이동 가능
-	MyPlayer.Move(10, 10);
-	MyPlayer.Move(DestX, DestY);
-	MyBoar.Move(DestX, DestY);
-	MySlime.Move(DestX, DestY);
-	MyGoblin.Move(DestX, DestY);
-
-	// 공격 가능
-	MyPlayer.Attack(MyBoar);
-	MyBoar.Attack(MyPlayer);
-	MySlime.Attack(MyPlayer);
-	MyGoblin.Attack(MyPlayer);
-
-	// 플레이어 골드 습득 가능
-	int CurrentGold = MyPlayer.GetGold();
-	MyPlayer.SetGold(CurrentGold + 10);
+	for (auto data : IntArray)
+	{
+		cout << data << endl;
+	}
 
 	return 0;
 }
-
-// 세상이 있습니다. 세상에는 플레이어와
-// 몬스터가 있습니다. 몬스터는 고블린, 슬라임, 멧돼지가
-// 있습니다. 고블린은 달릴 수 잇고 공격 가능합니다.
-// 슬라임은 미끄러지고 공격 가능합니다.
-// 멧돼지는 들이박고 공격이 가능합니다.
-// 플레이어는 HP와 MP가 있고 공격이 가능하고
-// 몬스터를 잡아서 골드를 모을 수 있습니다.
 
---
+++ UWorld.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class APlayer;
+class ASlime;
+class AGoblin;
+class ABoar;
+
+class UWorld
+{
+public:
+	APlayer* player;
+	ASlime* Slime;
+	AGoblin* Goblin;
+	ABoar* Boar;
+};
+
